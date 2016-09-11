6a4159dac69f470c7a272e10da638da5acb678a7
Mariza Pereira <marupereira@ladmins-macbook.attlocal.net>
2016-09-10 20:27:10 -0700

    First commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+ var today = new Date ();
+    var hourNow = today.getHours();
+    var greeting;
+
+    if (hourNow > 18){
+        greeting ='Good Evening, ';
+    }else if (hourNow > 12){
+        greeting ='Good Afternoon, ';
+    }else if (hourNow > 0){
+        greeting ='Good Morning, ';
+    }else{
+        greeting ='Hello! ';
+    }
+document.getElementById('timeOfDay').innerText = greeting + "welcome to the Election Day Countdown homepage!";
+
+
+
 $(document).ready(function(){
 
 	$('.my-countdown').countdown({
@@ -5,3 +22,5 @@ $(document).ready(function(){
 			-8, 2016, 10, 7, 0, 0, 0)
 	});
 });
+
+
